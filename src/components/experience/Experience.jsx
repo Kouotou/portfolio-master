e5b18c4d17e82b837cd408caf6005d11974c0941
Kouotou Ahmad Bilal <kouotouahmad@gmail.com>
2023-06-05 16:58:35 +0100

    Done with the front end

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./experience.css";
+//import { BsPatchCheckFill } from "react-icons/bs";
 import ListExperiences from "./components/listExperiences";
 
 export const Experience = () => {
@@ -7,7 +8,18 @@ export const Experience = () => {
     <section id="experience">
       <h5>What skills i have</h5>
       <h2>My Experience</h2>
+
       <div className="container experience__container">
+        {/* <div className="experience_frontend">
+          <h3> Frontend Development</h3>
+          <div className="experience_content">
+              <article className="experience_details">
+                <BsPatchCheckFill />
+                <h4>HTML</h4>
+                <small className="text-light"> Experienced</small>
+              </article>
+          </div>
+        </div> */}
         <ListExperiences />
       </div>
     </section>
--- src/components/experience/components/listExperiences.js
+++ src/components/experience/components/listExperiences.js
@@ -18,16 +18,16 @@ const ListExperiences = () => {
   return data.map((d) => {
     return (
       <div key={d._id} className="experience__backend">
-        <h3>{d.title}</h3>
+        <h3>Latest Experience</h3>
         <h4 className="experience_description">{d.description}</h4>
         <div className="experience__content">
           {d.skills.map((s) => {
             return (
-              <article key={s._id} className="experience__details">
+              <article key='60' className="experience__details">
                 <BsPatchCheckFill className="experience__details__icon" />
                 <div className="title__level">
-                  <h4>{s.name}</h4>
-                <small className="text-light">{s.level}</small>
+                  <h4>Ahmad Bilal</h4>
+                <small className="text-light">level 300</small>
                 </div>
               </article>
             );
